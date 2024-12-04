a1ab632eaba8f6a93cded6c1646ba66956a70c9d
Aymen Hamim <aymenhamim2004@gmail.com>
2024-12-04 13:27:38 +0100

    TypeScript: Finish Classes

--- TypeScript/Section 3/src/app.ts
+++ TypeScript/Section 3/src/app.ts
@@ -1,10 +1,14 @@
 class Department {
   // private id: string;
   // name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.name = n;
+  }
+
+  static createEmployee(name: string) {
+    return { name };
   }
 
   describe(this: Department) {
@@ -28,18 +32,45 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
   constructor(id: string, private reports: string[]) {
     super(id, "accounting");
+    this.lastReport = reports[0];
+  }
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No Report Found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      return;
+    }
+    this.addReport(value);
+  }
+
+  addEmploye(name: string) {
+    if (name == "Max") {
+      return;
+    }
+    this.employees.push(name);
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
     console.log(this.reports);
   }
 }
+
+const employee1 = Department.createEmployee("aymen");
+console.log(employee1);
 
 const it = new ITDepartment("a1", ["eric"]);
 
@@ -56,6 +87,11 @@ console.log(it);
 const accounting = new AccountingDepartment("b1", []);
 
 accounting.addReport("Test report");
+accounting.addEmploye("Maxx");
 accounting.printReports();
+
+accounting.mostRecentReport = "Eric";
+
+console.log(accounting.mostRecentReport);
 
 console.log(accounting);
