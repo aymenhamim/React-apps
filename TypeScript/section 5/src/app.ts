1a58c86fd78e86426f3fd3feee81bf49d0558344
Aymen Hamim <aymenhamim2004@gmail.com>
2024-12-04 23:53:09 +0100

    Creating a Generic Function

--- TypeScript/section 5/src/app.ts
+++ TypeScript/section 5/src/app.ts
@@ -1,11 +1,23 @@
-const names: Array<string> = ["Max", "Robert"];
+// const names: Array<string> = ["Max", "Robert"];
 
-names[0].length;
+// names[0].length;
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 1000);
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 1000);
+// });
 
-promise.then((data) => {});
+// promise.then((data) => {
+// data.split(' ')
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const MergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
+  { name: "aymen", hobbies: ["sport"] },
+  { age: 20 }
+);
+console.log(MergedObj);
