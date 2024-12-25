33ff4908a8469620b77dc98208a33aafc441f40b
aymenhamim <aymenhamim2004@gmail.com>
2024-12-25 23:35:27 +0100

    Handling New answer

--- quiz/src/Options.jsx
+++ quiz/src/Options.jsx
@@ -1,8 +1,21 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
-      {question.options.map((o) => (
-        <button key={o} className="btn btn-option">
+      {question.options.map((o, i) => (
+        <button
+          className={`btn btn-option ${i === answer ? "answer" : ""}
+          ${
+            hasAnswered
+              ? i === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={o}
+          onClick={() => dispatch({ type: "newAnswer", payload: i })}
+          disabled={hasAnswered}
+        >
           {o}
         </button>
       ))}
