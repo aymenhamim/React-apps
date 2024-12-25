4e1201a8bfb851bb96fc5d522c246d227793fd58
Aymen Hamim <aymenhamim2004@gmail.com>
2024-12-25 08:09:59 +0100

    settup react quiz

---
+++ quiz/src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./AppMain";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
