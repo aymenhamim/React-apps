e08f37120357dd4e770a94a301e4675333b5d246
Aymen Hamim <aymenhamim2004@gmail.com>
2024-12-29 13:40:04 +0100

    install redux-toolkit

--- small-bank-app/src/store-v2.js
+++ small-bank-app/src/store-v2.js
@@ -3,13 +3,9 @@ import { thunk } from "redux-thunk"
 import Accountreducer from "./features/accounts/AccountSlice"
 import customerReducer from "./features/customers/customerSlice"
 
-import { configureStore } from "@reduxjs/toolkit"
 
-const store = configureStore({
-    reducer: {
-        account: Accountreducer,
-        customer: customerReducer
-    }
-})
+const rootReducer = combineReducers({ account: Accountreducer, customer: customerReducer })
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export default store
