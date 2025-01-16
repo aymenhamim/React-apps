6807d0d37444fa3abf252ccd869fb00c9fd7f2f9
Aymen Hamim <aymenhamim2004@gmail.com>
2025-01-16 18:50:44 +0100

    Install React Router

--- e-commerce/front-end/src/App.tsx
+++ e-commerce/front-end/src/App.tsx
@@ -1,15 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import React from 'react';
+import UserLayout from './layouts/userLayout';
+import Category from './pages/Category';
+import ContactUs from './pages/ContactUs';
 
-function App() {
+const App: React.FC = () => {
   // const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <SignIn />
-      <SignUp />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index path="home" element={<Home />} />
+          <Route path="category" element={<Category />} />
+          <Route path="contact-us" element={<ContactUs />} />
+        </Route>
+        <Route path="login" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
