d24fbe1101903a86b9420fce651428624b70721b
aymen hamim <aymenhamim2004@gmail.com>
2025-01-31 20:34:57 +0100

    tracking element wit FM

--- framer-motion/src/App.jsx
+++ framer-motion/src/App.jsx
@@ -1,40 +1,26 @@
-import { motion, useScroll } from 'framer-motion';
-import { useRef } from 'react';
+import { motion, useScroll, useMotionValueEvent } from 'framer-motion';
+import { div } from 'framer-motion/client';
+import { useState, useRef } from 'react';
 
-// A small placeholder component to give us some room to scroll.
-const Placeholder = () => (
-  <div className="aspect-video w-[600px] max-w-[80%] shrink-0 rounded-lg bg-slate-300 opacity-30" />
-);
-
-export const App = () => {
+const App = () => {
   const ref = useRef(null);
-  const { scrollXProgress } = useScroll({
-    container: ref,
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start end', 'end start'],
   });
+  const [y, setY] = useState(0);
+
+  useMotionValueEvent(scrollYProgress, 'change', y => setY(y.toFixed(2)));
 
   return (
-    <main className="bg-slate-700 p-2">
-      <div className="sticky top-0 z-10 m-2 bg-gray-300">
-        <motion.div
-          style={{ scaleX: scrollXProgress }}
-          className="h-3 w-full origin-left bg-[#bc9e7a]"
-        />
-      </div>
-      <p className="my-6 text-white">
-        Time to make some progress and start scrolling... 😉
-      </p>
-      <div ref={ref} className="overflow-x-auto py-2">
-        <div className="flex gap-2">
-          <Placeholder />
-          <Placeholder />
-          <Placeholder />
-          <Placeholder />
-          <Placeholder />
-          <Placeholder />
-          <Placeholder />
-        </div>
-      </div>
-    </main>
+    <div className="flex min-h-[300vh] justify-center bg-stone-900 text-2xl text-white">
+      <div className="fixed top-0">{y}</div>
+
+      <div
+        className="mt-[110vh] h-[50vh] w-1/2 rounded-2xl bg-gray-300"
+        ref={ref}
+      ></div>
+    </div>
   );
 };
 
