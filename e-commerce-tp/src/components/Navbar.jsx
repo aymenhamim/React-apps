e1c47dac09490f2c0269b514e6c84207e25b5dd9
aymen hamim <aymenhamim2004@gmail.com>
2025-02-02 19:32:09 +0100

    implement layout animation in navbar

--- e-commerce-tp/src/components/Navbar.jsx
+++ e-commerce-tp/src/components/Navbar.jsx
@@ -2,29 +2,72 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 import IconButton from './IconButton';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(1);
 
   function handleClick() {
     setIsOpen(!isOpen);
   }
   function handleLogout() {}
+  console.log(activeIndex);
 
   return (
     <div className="mx-auto flex w-[90dvw] items-center justify-between rounded-lg border border-stone-600 bg-stone-50 px-5 py-2.5 shadow-sm">
-      <ul className="flex gap-3">
+      <ul className="flex items-center gap-3">
         <li>
           <span className="mr-5 font-bold">SHOP</span>
         </li>
-        <li>
-          <NavLink to={'/products'}>Products</NavLink>
+
+        <li className="relative px-3 py-2">
+          {activeIndex === 1 && (
+            <motion.span
+              layoutId="navlink"
+              className="absolute inset-0 block rounded-full bg-stone-900"
+            ></motion.span>
+          )}
+          <NavLink
+            to={'/products'}
+            className="relative"
+            onClick={() => setActiveIndex(1)}
+            // style={{ color: activeIndex === 1 ? '#ffffff' : '#000000' }}
+          >
+            Products
+          </NavLink>
         </li>
-        <li>
-          <NavLink to={'/Category'}>Category</NavLink>
+
+        <li className="relative px-3 py-2">
+          {activeIndex === 2 && (
+            <motion.span
+              layoutId="navlink"
+              className="absolute inset-0 block rounded-full bg-stone-900"
+            ></motion.span>
+          )}
+          <NavLink
+            to={'/Category'}
+            onClick={() => setActiveIndex(2)}
+            className="relative"
+          >
+            Category
+          </NavLink>
         </li>
-        <li>
-          <NavLink to={'/Contact-us'}>Contact us</NavLink>
+
+        <li className="relative px-3 py-2">
+          {activeIndex === 3 && (
+            <motion.span
+              layoutId="navlink"
+              className="absolute inset-0 block rounded-full bg-stone-900"
+            ></motion.span>
+          )}
+          <NavLink
+            to={'/Contact-us'}
+            onClick={() => setActiveIndex(3)}
+            className="relative"
+          >
+            Contact us
+          </NavLink>
         </li>
       </ul>
       <div className="flex items-center gap-2">
--- e-commerce-tp/src/layouts/UserLayout.jsx
+++ e-commerce-tp/src/layouts/UserLayout.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar';
 import { Outlet, useLocation } from 'react-router-dom';
 import Footer from '../components/Footer';
+import { useSelector } from 'react-redux';
 
 const UserLayout = () => {
   const location = useLocation();
   const isProduct = location.pathname === '/products';
+  const { isLoading } = useSelector(state => state.products);
 
   return (
     <div className="min-h-dvh min-w-full bg-slate-100 pt-4">
@@ -12,7 +14,7 @@ const UserLayout = () => {
       <div className="mx-auto mt-4 flex w-[90dvw]">
         <Outlet />
       </div>
-      {isProduct && <Footer />}
+      {isProduct && !isLoading && <Footer />}
     </div>
   );
 };
