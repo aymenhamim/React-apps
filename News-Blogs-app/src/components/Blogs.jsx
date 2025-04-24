9baaf60e1afcb9d6b77c346e9875a84bd366d9d8
aymen hamim <aymenhamim2004@gmail.com>
2025-04-24 22:02:44 +0100

    Creating & customizing the form

--- News-Blogs-app/src/components/Blogs.jsx
+++ News-Blogs-app/src/components/Blogs.jsx
@@ -7,7 +7,30 @@ function Blogs() {
         <img src="/public/images/user.jpg" alt="User Image" />
       </div>
       <div className="blogs-right">
-        <button className="post-btn">Create New Post</button>
+        {/* <button className="post-btn">Create New Post</button> */}
+
+        <div className="blogs-right-form">
+          <h1>New Post</h1>
+          <form action="">
+            <div className="img-upload">
+              <label htmlFor="file-upload" className="file-upload">
+                <i className="bx bx-upload"></i>Upload Image
+              </label>
+              <input type="file" id="file-upload" />
+            </div>
+
+            <input
+              type="text"
+              placeholder="Add Title (max 70 Characters)"
+              className="title-input"
+            />
+            <textarea className="text-input" placeholder="Add Text"></textarea>
+            <button type="submit" className="submit-btn">
+              Submit Button
+            </button>
+          </form>
+        </div>
+
         <button className="blogs-close-btn">
           Back <i className="bx bx-chevron-right"></i>
         </button>
