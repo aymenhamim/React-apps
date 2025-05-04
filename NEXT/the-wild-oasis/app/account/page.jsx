d6d69dca39174a1b7119e1dacc633f72a95a9840
aymen hamim <aymenhamim2004@gmail.com>
2025-05-04 14:00:02 +0100

    Defining Routes and Pages

---
+++ NEXT/the-wild-oasis/app/account/page.jsx
@@ -0,0 +1,9 @@
+function account() {
+  return (
+    <div>
+      <h1>Account Page</h1>
+    </div>
+  );
+}
+
+export default account;
---
+++ NEXT/the-wild-oasis/app/cabins/page.jsx
@@ -0,0 +1,9 @@
+function cabins() {
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+    </div>
+  );
+}
+
+export default cabins;
