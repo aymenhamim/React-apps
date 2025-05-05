641d98f0f32e9041bfe8d8cd1a6053388d919579
aymen hamim <aymenhamim2004@gmail.com>
2025-05-05 16:36:42 +0100

    Adding nested layout

--- NEXT/the-wild-oasis/app/account/profile/page.jsx
+++ NEXT/the-wild-oasis/app/account/profile/page.jsx
@@ -1,4 +1,9 @@
 import SelectCountry from "@/app/_components/SelectCountry";
+import Image from "next/image";
+
+export const metadata = {
+  title: "Update profile",
+};
 
 export default function Page() {
   // CHANGE
