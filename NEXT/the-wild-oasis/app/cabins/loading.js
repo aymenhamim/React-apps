28dce9e603add17f9e523030ecd948505fc1dc93
aymen hamim <aymenhamim2004@gmail.com>
2025-05-05 20:09:48 +0100

    Streaming route segments with loading.js

--- NEXT/the-wild-oasis/app/cabins/loading.js
+++ NEXT/the-wild-oasis/app/cabins/loading.js
@@ -2,8 +2,9 @@ import Spinner from "@/app/_components/Spinner";
 
 function Loading() {
   return (
-    <div>
+    <div className="grid itmes-center justify-center">
       <Spinner />
+      <p className="text-center text-gray-500">Loading cabin data...</p>
     </div>
   );
 }
