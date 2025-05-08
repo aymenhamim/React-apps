464c570fa42c2a02824d0476b53dc28c418cec89
aymen hamim <aymenhamim2004@gmail.com>
2025-05-08 15:41:44 +0100

    Getting the User Session

--- NEXT/the-wild-oasis/app/_lib/auth.js
+++ NEXT/the-wild-oasis/app/_lib/auth.js
@@ -5,7 +5,7 @@ const authConfig = {
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
-      clientId: process.env.AUTH_GOOGLE_SECRET,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
 };
