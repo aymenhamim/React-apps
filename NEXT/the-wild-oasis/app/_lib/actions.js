a2b9e293f07476b7121691485c78d52942fc91f8
aymen hamim <aymenhamim2004@gmail.com>
2025-05-08 16:35:19 +0100

    Building a Custom Sign Out Button

--- NEXT/the-wild-oasis/app/_lib/actions.js
+++ NEXT/the-wild-oasis/app/_lib/actions.js
@@ -1,9 +1,15 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 export async function signInAction() {
   await signIn("google", {
     redirectTo: "/account", // redirect to account page after sign in
+  });
+}
+
+export async function signOutAction() {
+  await signOut({
+    redirectTo: "/",
   });
 }
