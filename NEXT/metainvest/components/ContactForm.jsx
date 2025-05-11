c6f4f8af01924fc8b436b5d61a9396385dc6a928
aymen hamim <aymenhamim2004@gmail.com>
2025-05-11 19:38:02 +0100

    form validation with zod

--- NEXT/metainvest/components/ContactForm.jsx
+++ NEXT/metainvest/components/ContactForm.jsx
@@ -1,14 +1,31 @@
 "use client";
 
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Label } from "@radix-ui/react-label";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 
+const formSchema = z.object({
+  email: z.string().email({ message: "Adresse e-mail invalide" }),
+  telephone: z
+    .string()
+    .min(10)
+    .max(15, { message: "Numéro de téléphone invalide" }),
+  adresse: z.string().min(5, { message: "Adresse invalide" }),
+  text: z.string().min(10, { message: "Message trop court" }),
+});
+
 function ContactForm() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(formSchema) });
 
   const onSubmit = (data) => {
     console.log(data);
@@ -26,22 +43,34 @@ function ContactForm() {
           <div>
             <Label htmlFor="email">Email</Label>
             <Input type={"email"} {...register("email")} />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
           </div>
           <div>
             <Label htmlFor="telephone">Telephone</Label>
             <Input type={"nmber"} {...register("telephone")} />
+            {errors.telephone && (
+              <p className="text-red-500 text-sm">{errors.telephone.message}</p>
+            )}
           </div>
           <div>
             <Label htmlFor="adresse">Adresse</Label>
             <Input type={"text"} {...register("adresse")} />
+            {errors.adresse && (
+              <p className="text-red-500 text-sm">{errors.adresse.message}</p>
+            )}
           </div>
           <div>
             <Label htmlFor="text">Comment pouvons-nous vous aider ?</Label>
-            <Textarea {...register("text")} id="text" />
+            <Textarea {...register("text")} />
+            {errors.text && (
+              <p className="text-red-500 text-sm">{errors.text.message}</p>
+            )}
           </div>
 
           <div>
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full cursor-pointer">
               Envoyee
             </Button>
           </div>
