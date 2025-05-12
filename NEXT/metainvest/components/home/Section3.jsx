e8adde714cf824f389e34b3889510f95b31c96f9
aymen hamim <aymenhamim2004@gmail.com>
2025-05-12 12:51:53 +0100

    Finish Section 3

--- NEXT/metainvest/components/home/Section3.jsx
+++ NEXT/metainvest/components/home/Section3.jsx
@@ -1,25 +1,26 @@
 import capacity1 from "@/public/images/project-imgs/home/capacity1.jpg";
 import capacity2 from "@/public/images/project-imgs/home/capacity2.jpg";
 import capacity3 from "@/public/images/project-imgs/home/capacity3.jpg";
+import { CogIcon, HammerIcon, CircleArrowLeftIcon } from "lucide-react";
 import Image from "next/image";
 
 const capacities = [
   {
     title: "Usinage, Tournage et Meulage",
     text: "Nous fabriquons des pièces métalliques simples à moyennes en utilisant différentes techniques d’usinage manuel ou mécanique : fraisage, tournage et meulage selon les besoins du projet.",
-    icon: "xsq",
+    icon: <CogIcon size={35} />,
     img: capacity1,
   },
   {
     title: "Fabrication Métallique de Petite Série",
     text: "Nous réalisons des structures métalliques légères comme des chaises, des bancs ou des cadres, souvent destinées à l’usage scolaire ou au transport, en respectant les normes de solidité et de confort.",
-    icon: "xsq",
+    icon: <HammerIcon size={35} />,
     img: capacity2,
   },
   {
     title: "Finitions",
     text: "Nous collaborons avec des partenaires pour appliquer des traitements de surface adaptés : peinture, zingage, nickelage, ou d’autres finitions sur demande.",
-    icon: "xsq",
+    icon: <CircleArrowLeftIcon size={35} />,
     img: capacity3,
   },
 ];
@@ -29,12 +30,12 @@ function Section3() {
     <div
       className="bg-white min-h-[80dvh] py-32 
       flex flex-col items-center justify-center 
-      px-[15dvw] max-xl:px-[10dvw] max-lg:px-[5dvw] max-md:px-4 
-      gap-10 min-xl:gap-20"
+        max-lg:px-[5dvw] max-md:px-4 
+      gap-6 "
     >
       <h3
         className="text-center font-bold px-2 py-1 
-        border-b-3 border-red-600 max-md:border-0
+        border-b-2 border-red-600 max-md:border-0
          w-fit mx-auto
         text-5xl max-xl:text-4xl max-lg:text-3xl 
         "
@@ -42,24 +43,36 @@ function Section3() {
         Nos capacités de fabrication
       </h3>
 
-      <div className="flex max-md:flex-col w-[80dvw] max-lg:w-[90dvw] mx-auto gap-2 my-20  items-center max-md:gap-8 justify-center">
-        {capacities.map((capacity) => (
-          <div className="w-92 h-92  bg-stone-800 rounded-md relative font-semibold  overflow-hidden">
-            <h3 className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center z-10">
-              {capacity.title}
-            </h3>
-
-            <p className="absolute left-1/2 top-[80%] transform -translate-x-1/2 -translate-y-1/2 text-white text-center z-10">
-              {capacity.text}
-            </p>
+      <div className="flex max-md:flex-col w-[80dvw] max-lg:w-[90dvw] mx-auto gap-6   items-center justify-center py-14 max-md:gap-15">
+        {capacities.map((capacity, index) => (
+          <div key={index} className="w-96 aspect-square relative">
+            {/*  Capacity Image */}
 
             <Image
+              alt={capacity.title}
               src={capacity.img}
-              alt="main image"
+              className="object-cover brightness-30 rounded-2xl"
               fill
-              priority
-              className="object-cover brightness-[46%] max-md:object-bottom-left hover:scale-110 hover:saturate-50 hover:rotate-3 transition-all duration-200"
+              quality={100}
             />
+
+            {/*  Capacity Icon */}
+
+            <div className="absolute top-0 left-1/2 transform -translate-1/2 w-18 aspect-square rounded-full bg-stone-50 border border-stone-900 z-20 flex items-center justify-center shadow-2xl ">
+              {capacity.icon}
+            </div>
+
+            {/*  Capacity Data */}
+
+            <div className="flex flex-col justify-center items-center p-4 h-full gap-10 ">
+              <h3 className="text-center text-white font-bold text-shadow-lg text-xl z-10">
+                {capacity.title}
+              </h3>
+
+              <p className="text-sm text-center text-shadow-md text-white z-10">
+                {capacity.text}
+              </p>
+            </div>
           </div>
         ))}
       </div>
