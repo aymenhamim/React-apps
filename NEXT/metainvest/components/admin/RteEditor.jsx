8ec279a2be2d0c9be453ff58acd002f607b2b8fa
aymen hamim <aymenhamim2004@gmail.com>
2025-05-23 18:44:34 +0100

    create Text-editor

--- NEXT/metainvest/components/admin/RteEditor.jsx
+++ NEXT/metainvest/components/admin/RteEditor.jsx
@@ -1,17 +1,154 @@
-import { useEditor, EditorContent } from "@tiptap/react";
+"use client";
+
+import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
-function Tiptap() {
+import Heading from "@tiptap/extension-heading";
+import Underline from "@tiptap/extension-underline";
+import {
+  BoldIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  Heading4Icon,
+  ItalicIcon,
+  ListIcon,
+  ListOrderedIcon,
+  UnderlineIcon,
+} from "lucide-react";
+import "./RteEditorStyles.css";
+
+function Tiptap({ title = "Editor" }) {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Underline,
+      Heading.configure({ levels: [1, 2, 3, 4] }),
+    ],
     content: "<p>Hello World! 🌍️</p>",
+
     editorProps: {
       attributes: {
-        class: "prose prose-sm m-0 focus:outline-none",
+        class:
+          "prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl focus:outline-none min-h-[250px] min-w-[300px]",
       },
     },
+    immediatelyRender: false,
   });
-  return <div></div>;
+
+  if (!editor) {
+    return null;
+  }
+
+  const saveContent = () => {
+    if (editor) {
+      console.log(editor.getHTML());
+    }
+  };
+
+  return (
+    <div className="flex flex-col w-full">
+      <div className="w-full py-2">
+        <h2 className="mb-5">{title}</h2>
+
+        <div className="flex space-x-2 my-4">
+          {/* Heading */}
+
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+            className={`border border-stone-500 cursor-pointer rounded-md p-1 ${editor.isActive("heading", { level: 1 }) ? "bg-gray-300" : ""}`}
+          >
+            <Heading1Icon />
+          </button>
+
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 2 }).run()
+            }
+            className={`border border-stone-500 cursor-pointer rounded-md p-1 ${editor.isActive("heading", { level: 2 }) ? "bg-gray-300" : ""}`}
+          >
+            <Heading2Icon />
+          </button>
+
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 3 }).run()
+            }
+            className={`border border-stone-500 cursor-pointer rounded-md p-1 ${editor.isActive("heading", { level: 3 }) ? "bg-gray-300" : ""}`}
+          >
+            <Heading3Icon />
+          </button>
+
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 4 }).run()
+            }
+            className={`border border-stone-500 cursor-pointer rounded-md p-1${editor.isActive("heading", { level: 4 }) ? "bg-gray-300" : ""}`}
+          >
+            <Heading4Icon />
+          </button>
+
+          {/* Bold */}
+          <button
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={`border border-stone-500 cursor-pointer rounded-md p-1  ml-3 ${editor.isActive("bold") ? "bg-gray-300" : ""}`}
+          >
+            <BoldIcon />
+          </button>
+
+          {/* ITALIC */}
+          <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={`border border-stone-500  cursor-pointer rounded-md p-1 ${editor.isActive("italic") ? "bg-gray-300" : ""}`}
+          >
+            <ItalicIcon />
+          </button>
+
+          {/* Underline */}
+          <button
+            onClick={() => editor.chain().focus().toggleUnderline().run()}
+            className={`border border-stone-500  cursor-pointer rounded-md p-1 ${
+              editor.isActive("underline") ? "bg-gray-300" : ""
+            }`}
+          >
+            <UnderlineIcon />
+          </button>
+
+          {/* List */}
+          <button
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={`border border-stone-500  cursor-pointer rounded-md  p-1 ${
+              editor.isActive("bulletList") ? "bg-gray-300" : ""
+            }`}
+          >
+            <ListIcon />
+          </button>
+
+          {/* Ordered List */}
+
+          <button
+            onClick={() => editor.chain().focus().toggleOrderedList().run()}
+            className={`border border-stone-500  cursor-pointer rounded-md p-1 ${
+              editor.isActive("orderedList") ? "bg-gray-300" : ""
+            }`}
+          >
+            <ListOrderedIcon />
+          </button>
+        </div>
+
+        <EditorContent editor={editor} className="border rounded-lg p-2" />
+
+        <button
+          onClick={saveContent}
+          className="mt-2 p-2 bg-blue-500 text-white rounded"
+        >
+          Save
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Tiptap;
--- NEXT/metainvest/components/admin/ViewProduct.jsx
+++ NEXT/metainvest/components/admin/ViewProduct.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import Modal from "../Modal";
+import Tiptap from "./RteEditor";
 
 function ViewProduct({ children }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,10 +15,10 @@ function ViewProduct({ children }) {
       </Button>
 
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center min-w-[30rem]">
           <h2 className="text-2xl font-bold my-5">View Product</h2>
 
-          <div></div>
+          <Tiptap />
         </div>
       </Modal>
     </div>
