b8f04f283756ff4fd84301efa7422dc81e805d3c
Aymen Hamim <aymenhamim2004@gmail.com>
2025-10-11 00:23:56 +0100

    Refactor RecentTransactions component to use RecentTransactionList for rendering transactions; update layout in HomePage

--- NEXT/banking-system/app/home/page.tsx
+++ NEXT/banking-system/app/home/page.tsx
@@ -2,7 +2,7 @@ import RecentTransactions from "@/components/widgets/recent-transactions";
 
 function HomePage() {
   return (
-    <div className="flex flex-col lg:flex-row gap-6 max-w-screen-xl mx-auto p-4 ">
+    <div className="flex flex-col lg:flex-row gap-6 max-w-screen-2xl mx-auto p-4 ">
       <RecentTransactions />
       <main className="flex-1 flex flex-col gap-6 bg-green-500">dsqdqs</main>
     </div>
---
+++ NEXT/banking-system/components/widgets/recent-transactions/recent-transaction-list.tsx
@@ -0,0 +1,18 @@
+import { Transaction } from "@/types/transactions";
+import RecentTransactionItem from "./recent-transaction-item";
+
+interface RecentTransactionListType {
+  transactions: Transaction[];
+}
+
+function RecentTransactionList({ transactions }: RecentTransactionListType) {
+  return (
+    <div className="">
+      {transactions.map((tr) => (
+        <RecentTransactionItem transaction={tr} key={tr.id} />
+      ))}
+    </div>
+  );
+}
+
+export default RecentTransactionList;
