03faabdaa3b4730ec46a4d68857da2fb597bbaa9
Aymen Hamim <aymenhamim2004@gmail.com>
2025-10-11 23:56:41 +0100

    refactor: update TransactionsPage layout to enforce minimum height; remove unused SelectLabel import in TransactionsFilter

--- NEXT/banking-system/app/transactions/page.tsx
+++ NEXT/banking-system/app/transactions/page.tsx
@@ -4,7 +4,7 @@ import TransactionsHeader from "@/components/widgets/transactions/transactions-h
 
 function TransactionsPage() {
   return (
-    <div className="max-w-screen-2xl mx-auto mt-5  bg-white rounded-2xl relative max-h-[87dvh] flex flex-col overflow-hidden">
+    <div className="max-w-screen-2xl mx-auto mt-5 bg-white rounded-2xl relative min-h-[65dvh] max-h-[87dvh] flex flex-col overflow-hidden">
       <div className="overflow-y-auto flex-1 px-4 py-2 lg:px-20 lg:pt-6">
         <TransactionsHeader />
         <Transactions />
