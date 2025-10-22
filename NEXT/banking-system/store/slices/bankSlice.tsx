644bd8165587320429f9014b6c8344f55d0f630c
Aymen Hamim <aymenhamim2004@gmail.com>
2025-10-22 23:38:16 +0100

    feat: refactor bankSlice; update transaction fetching logic, enhance loading state management, and clean up unused code

--- NEXT/banking-system/store/slices/bankSlice.tsx
+++ NEXT/banking-system/store/slices/bankSlice.tsx
@@ -4,41 +4,51 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const API_BASE_URL = "http://127.0.0.1:8000/";
 
+interface fetchTransactionsType {
+  customer?: string;
+  type?: string;
+  limit?: string;
+}
+
 const initialState = {
-  user: null,
   account: null,
   filteredTransactions: [],
   recentTransactions: [],
   isAuth: false,
-  isLoading: false,
+  loading: false,
   error: null,
 };
 
-// export const getTransactions = createAsyncThunk(
-//   "transactions/get",
-//   async (page, thunkAPI) => {
-//     try {
-//       //   const res = await fetch(`${API_BASE_URL}products?page=${page}`);
-//       const res = await fetchTransactions({});
-
-//       if (!res.ok) {
-//         throw new Error("Failed to fetch products");
-//       }
-
-//       const data = await res.json();
-
-//       return data;
-//     } catch (error) {
-//       console.log(error.message);
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
+export const getTransactions = createAsyncThunk(
+  "transactions/get",
+  async ({ limit = "10", type, customer }: fetchTransactionsType, thunkAPI) => {
+    const res = await api.get(
+      `http://localhost:8000/api/transactions?customer=${customer}&type=${type}&limit=${limit}`
+    );
+    return res;
+  }
+);
 
 const bankSlice = createSlice({
   name: "bank",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTransactions.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getTransactions.fulfilled, (state, action) => {
+        state.loading = false;
+        state.recentTransactions = action.payload.data;
+        state.error = null;
+      })
+      .addCase(getTransactions.rejected, (state) => {
+        state.loading = false;
+        // state.error = null;
+      });
+  },
 });
 
 export default bankSlice.reducer;
