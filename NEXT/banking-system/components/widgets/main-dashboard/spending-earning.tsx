affe626560f8c06edf7d8211a936e40774b2616a
Aymen Hamim <aymenhamim2004@gmail.com>
2025-10-23 18:56:38 +0100

    refactor: improve responsive design in SpendingEarning component; adjust class names for better layout and visibility

--- NEXT/banking-system/components/widgets/main-dashboard/spending-earning.tsx
+++ NEXT/banking-system/components/widgets/main-dashboard/spending-earning.tsx
@@ -6,15 +6,15 @@ interface SpendingEarningType {
 
 function SpendingEarning({ type, date, amount }: SpendingEarningType) {
   return (
-    <div className="bg-white rounded-2xl h-full px-8 py-2 flex justify-between items-center">
+    <div className="bg-white rounded-2xl h-full px-1 md:px-8 py-2 flex max-sm:flex-col min-md:justify-between items-center">
       <div className="flex flex-col justify-around h-full">
         <div>
-          <h4 className="text-lg font-semibold">Total {type}</h4>
-          <p className="text-sm font-semibold">{date}</p>
+          <h4 className="text-lg font-semibold max-sm:text-sm">Total {type}</h4>
+          <p className="text-sm font-semibold max-sm:hidden">{date}</p>
         </div>
-        <h3 className="text-xl font-bold">{amount}</h3>
       </div>
-      <div className="w-32 h-20 bg-indigo-100 rounded-2xl border border-indigo-300"></div>
+      <h3 className="text-xl font-bold max-sm:text-md">{amount}</h3>
+      {/* <div className="w-32 h-20 bg-indigo-100 rounded-2xl border border-indigo-300"></div> */}
     </div>
   );
 }
