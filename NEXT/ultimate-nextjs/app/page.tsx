2e82361e4244462e3c5c7336c9f601d66f323c78
Aymen Hamim <aymenhamim2004@gmail.com>
2025-12-08 14:14:06 +0100

    feat: add Space Grotesk font and update Home component to use new font styles

--- NEXT/ultimate-nextjs/app/page.tsx
+++ NEXT/ultimate-nextjs/app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
     <div className="w-dvw h-dvh flex justify-center">
-      <div className="px-20 py-10 mt-32 rounded-2xl flex items-center justify-center border h-fit bg-gray-100">
-        Tailwind CSS is fun!
+      <div className="px-20 py-10 mt-32 rounded-2xl flex items-center justify-center border h-fit bg-gray-100 flex-col">
+        <p>Tailwind CSS is fun!</p>
+
+        <p className="font-inter">Tailwind CSS is fun!</p>
+        <p className="font-space-grotesk ">Tailwind CSS is fun!</p>
       </div>
     </div>
   );
