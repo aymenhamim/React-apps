45c7e170c9b3ab0e0106bddcf78f1b98198ff9dd
Aymen Hamim <aymenhamim2004@gmail.com>
2025-12-10 14:47:26 +0100

    feat: implement authentication with NextAuth and GitHub provider

--- NEXT/ultimate-nextjs/app/(root)/page.tsx
+++ NEXT/ultimate-nextjs/app/(root)/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
     <div className="w-dvw h-dvh flex justify-center">
       <div className="px-20 py-10 mt-32 rounded-2xl flex items-center justify-center border h-fit  flex-col">
         <p>Tailwind CSS is fun!</p>
-
         <>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
