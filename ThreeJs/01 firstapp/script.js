65ec677f28d5f3cc0a61cd0607449f4ecd776587
Aymen Hamim <aymenhamim2004@gmail.com>
2025-12-31 23:45:05 +0100

    First Three js app

---
+++ ThreeJs/01 firstapp/script.js
@@ -0,0 +1,30 @@
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Create Scene
+const scene = new THREE.Scene();
+
+// Mesh
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: "#ff0000",
+});
+const mesh = new THREE.Mesh(geometry, material);
+
+// Add this mesh to the scene
+scene.add(mesh);
+
+const sizes = { width: 600, heigth: 400 };
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.heigth);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.heigth);
+renderer.render(scene, camera);
