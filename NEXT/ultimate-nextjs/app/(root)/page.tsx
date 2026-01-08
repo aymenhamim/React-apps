86266322213b08bf6eca1653842fab8121335e4d
Aymen Hamim <aymenhamim2004@gmail.com>
2026-01-08 16:03:46 +0100

    Implement Responsive Left sidebar

--- NEXT/ultimate-nextjs/app/(root)/page.tsx
+++ NEXT/ultimate-nextjs/app/(root)/page.tsx
@@ -3,7 +3,11 @@ import { auth } from "@/auth";
 const Home = async () => {
   const session = await auth();
   console.log(session);
-  return <div className="w-dvw h-dvh flex justify-center"></div>;
+  return (
+    <div>
+      <h2 className="text-xl font-bold">Welcome to the world of Next.js</h2>
+    </div>
+  );
 };
 
 export default Home;
