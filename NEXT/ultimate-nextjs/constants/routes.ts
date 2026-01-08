c199938834e4015d97040d1db7c4dad493dc714d
Aymen Hamim <aymenhamim2004@gmail.com>
2026-01-08 16:44:07 +0100

    feat: add dynamic routes for question, profile, and tags

--- NEXT/ultimate-nextjs/constants/routes.ts
+++ NEXT/ultimate-nextjs/constants/routes.ts
@@ -2,6 +2,9 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
+  QUESTION: (id: string) => `/profile/${id}`,
+  PROFILE: (id: string) => `/profile/${id}`,
+  TAGS: (id: string) => `/tags/${id}`,
 };
 
 export default ROUTES;
