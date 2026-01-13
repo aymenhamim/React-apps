24447b6b8df6935ced7f36d51379a387e6e020f0
Aymen Hamim <aymenhamim2004@gmail.com>
2026-01-13 17:08:41 +0100

    feat: Implement LocalSearch bar

--- NEXT/ultimate-nextjs/app/(root)/page.tsx
+++ NEXT/ultimate-nextjs/app/(root)/page.tsx
@@ -2,8 +2,56 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import LocalSearch from "@/components/search/LocalSearch";
 
-async function Home() {
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn react",
+    description: "I want to learn react, can anyone help me ?",
+    tags: [
+      {
+        _id: "1",
+        name: "React",
+      },
+      {
+        _id: "2",
+        name: "Javascript",
+      },
+    ],
+  },
+  {
+    _id: "2",
+    title: "How to learn NextJs",
+    description: "I want to learn NextJs, can anyone help me ?",
+    tags: [
+      {
+        _id: "1",
+        name: "NextJs",
+      },
+      {
+        _id: "2",
+        name: "Javascript",
+      },
+    ],
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+async function Home({ searchParams }: SearchParams) {
+  const { query } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
+
+  console.log();
+
+  //? const {data} = await axios.get("/api/questions", {query: {search: query}})
+
   return (
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -18,14 +66,18 @@ async function Home() {
         </Button>
       </section>
       <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        LocalSearch
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="cursor-pointer"
+        />
       </section>
-      HomeFilter
+      {/* HomeFilter */}
       <div className="mt-10 flex w-full flex-col gap-6">
-        <p>Qestion Card 1</p>
-        <p>Qestion Card 2</p>
-        <p>Qestion Card 3</p>
-        <p>Qestion Card 4</p>
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
       </div>
     </>
   );
